9d16eb522a9463e4af975295e8b93308bb1ba0ca
unknown <hackinghungtx@gmail.com>
2023-05-10 10:15:34 +0700

    PRN211

---
+++ ConsoleApp1 (2)/ConsoleApp1/Bai3.cs
@@ -0,0 +1,129 @@
+﻿ namespace ConsoleApp1
+ {
+     internal class Bai3
+     {
+         static void Main(string[] args)
+         {
+             Console.WriteLine("Image 1: ");
+             for(int i  = 0; i <= 4; i++)
+             {
+                 for (int j = 0; j <= i; j++)
+                 {
+                     Console.Write("* ");
+                 }
+                 Console.WriteLine();
+             }
+             Console.WriteLine("Image 2: ");
+             for (int i = 0; i <= 4; i++)
+             {
+                 for (int j = 0; j < 5-i; j++)
+                 {
+                     Console.Write("* ");
+                 }
+                 Console.WriteLine() ;
+             }
+             Console.WriteLine("Image 3: ");
+             for(int i = 0;i < 5; i++)
+             {
+                 for(int j = 0;j < 5; j++)
+                 {
+                     if(j < 5 - i - 1)
+                     {
+                         Console.Write("  ");
+                     }
+                     else
+                     {
+                         Console.Write("* ");
+                     }
+                    
+                 }
+                 Console.WriteLine();
+             }
+             Console.WriteLine("Image 4: ");
+             for (int i = 0; i < 5; i++)
+             {
+                 for (int j = 0; j < 5; j++)
+                 {
+                     if (j >= i)
+                     {
+                         Console.Write("* ");
+                     }
+                     else
+                     {
+                         Console.Write("  ");
+                     }
+
+                 }
+                 Console.WriteLine();
+             }
+             Console.WriteLine("Image 5: ");
+             for (int i = 0; i < 5; i++)
+             {
+                 for (int j = 0; j < 10; j++)
+                 {
+                     if (Math.Abs(5-j-1)<=i)
+                     {
+                         Console.Write("* ");
+                     }
+                     else
+                     {
+                         Console.Write("  ");
+                     }
+
+                 }
+                 Console.WriteLine();
+             }
+             Console.WriteLine("Image 6: ");
+             for (int i = 0; i < 5; i++)
+             {
+                 for (int j = 0; j < 10; j++)
+                 {
+                     if (j>i -1 && j< 10 - i- 1)
+                     {
+                         Console.Write("* ");
+                     }
+                     else
+                     {
+                         Console.Write("  ");
+                     }
+
+                 }
+                 Console.WriteLine();
+             }
+             Console.WriteLine("Image 7: ");
+             for (int i = 0; i < 10-1; i++)
+             {
+                 for (int j = 0; j < 10; j++)
+                 {
+                     if (Math.Abs(5-j-1) >Math.Abs(5-i-1) && Math.Abs(5-j) < 10 - Math.Abs(5 - i-1))
+                     {
+                         Console.Write("  ");
+                     }
+                     else
+                     {
+                         Console.Write("* ");
+                     }
+
+                 }
+                 Console.WriteLine();
+             }
+             Console.WriteLine("Image 8: ");
+             for (int i = 0; i < 7; i++)
+             {
+                 for (int j = 0; j < 12; j++)
+                 {
+                     if (i == 0 || i == 6 || j == 0 || j == 11 )
+                     {
+                         Console.Write("* ");
+                     }
+                     else
+                     {
+                         Console.Write("  ");
+                     }
+
+                 }
+                 Console.WriteLine();
+             }
+         }
+     }
+ }
---
+++ ConsoleApp1 (2)/ConsoleApp1/Bai8.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class Bai8
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student("HS001", "Nguyen Van A", 8, 8, 7, 8, 8));
+            students.Add(new Student("HS002", "Nguyen Van B", 8, 8, 7, 8, 8));
+            students.Add(new Student("HS003", "Nguyen Van C", 8, 8, 7, 8, 8));
+            students.Add(new Student("HS004", "Nguyen Van D", 8, 8, 7, 8, 8));
+            students.Add(new Student("HS005", "Nguyen Van E", 8, 8, 7, 8, 8));
+            students.Add(new Student("HS006", "Nguyen Van F", 8, 8, 7, 8, 8));
+            students.Add(new Student("HS007", "Nguyen Van G", 8, 8, 7, 8, 8));
+            students.Add(new Student("HS008", "Nguyen Van H", 8, 8, 7, 8, 8));
+
+            List<String> title = new List<String>();
+            title.Add(" STT");
+            title.Add("  Ma Hoc Sinh");
+            title.Add("      Ho Va Ten");
+            title.Add("      Diem He So");
+            title.Add("Diem TBKT");
+            title.Add("Diem Thi");
+            title.Add("Diem TBMH");
+            title.Add("   HS1");
+            title.Add("   HS2"); 
+            //
+            for (int i = 0;i < 5; i++)
+            {
+                if (i == 0)
+                {
+                    for (int j = 0; j < 100; j++)
+                    {
+                        Console.Write("-");
+                    }
+                    Console.WriteLine();
+                }else if (i == 4)
+                {
+                    Console.Write("|");
+                    for (int j = 0; j < 98; j++)
+                    {
+                        Console.Write("-");
+                    }
+                    Console.Write("|");
+                    Console.WriteLine();
+                }
+                else
+                {
+                  
+                        //if (j == 0 || j == 6 || j == 21 || j == 40 || j == 50 || j == 70 || j == 80 || j == 90 || j == 99)
+                        //{
+                        //    Console.Write("|");
+
+                        //}
+                        //else
+                        //{
+                        //    Console.Write(" ");
+                        //}
+                    if(i == 2)
+                    {
+                        Console.Write("|");
+                        Console.Write($"{title[0],-5}");
+                        Console.Write("|");
+                        Console.Write($"{title[1],-15}");
+                        Console.Write("|");
+                        Console.Write($"{title[2],-22}");
+                        Console.Write("|");
+                        Console.Write($"{"--------------------",-20}");
+                        Console.Write("|");
+                        Console.Write($"{title[4],-10}");
+                        Console.Write("|");
+                        Console.Write($"{title[5],-10}");
+                        Console.Write("|");
+                        Console.Write($"{title[6],-10}");
+                        Console.Write("|");
+                        Console.WriteLine();
+                    }
+                    if(i == 1)
+                    {
+                        Console.Write("|");
+                        Console.Write($"{"",-5}");
+                        Console.Write("|");
+                        Console.Write($"{"",-15}");
+                        Console.Write("|");
+                        Console.Write($"{"",-22}");
+                        Console.Write("|");
+                        Console.Write($"{title[3],-20}");
+                        Console.Write("|");
+                        Console.Write($"{"",-10}");
+                        Console.Write("|");
+                        Console.Write($"{"",-10}");
+                        Console.Write("|");
+                        Console.Write($"{"",-10}");
+                        Console.Write("|");
+                        Console.WriteLine();
+                    }
+                    if(i == 3)
+                    {
+                        Console.Write("|");
+                        Console.Write($"{"",-5}");
+                        Console.Write("|");
+                        Console.Write($"{"",-15}");
+                        Console.Write("|");
+                        Console.Write($"{"",-22}");
+                        Console.Write("|");
+                        Console.Write($"{title[7],-10}");
+                        Console.Write("|");
+                        Console.Write($"{title[8],-9}");
+                        Console.Write("|");
+                        Console.Write($"{"",-10}");
+                        Console.Write("|");
+                        Console.Write($"{"",-10}");
+                        Console.Write("|");
+                        Console.Write($"{"",-10}");
+                        Console.Write("|");
+                        Console.WriteLine();
+                    }
+                    
+                }
+                
+                
+            }
+            for(int i=0; i<students.Count;i++)
+            {
+                if(i == students.Count -1)
+                {
+                    Console.Write("|");
+                    Console.Write($"{i+1,-5}");
+                    Console.Write("|");
+                    Console.Write($"{students[i].id,-15}");
+                    Console.Write("|");
+                    Console.Write($"{students[i].name,-22}");
+                    Console.Write("|");
+                    Console.Write($"{students[i].HS1,-10}");
+                    Console.Write("|");
+                    Console.Write($"{students[i].HS2,-9}");
+                    Console.Write("|");
+                    Console.Write($"{students[i].tbkt,-10}");
+                    Console.Write("|");
+                    Console.Write($"{students[i].mark,-10}");
+                    Console.Write("|");
+                    Console.Write($"{students[i].tb,-10}");
+                    Console.Write("|");
+                    Console.WriteLine();
+                    for(int j = 0; j < 100; j++)
+                    {
+                        Console.Write("-");
+                    }
+                }
+                else
+                {
+                    Console.Write("|");
+                    Console.Write($"{i+1,-5}");
+                    Console.Write("|");
+                    Console.Write($"{students[i].id,-15}");
+                    Console.Write("|");
+                    Console.Write($"{students[i].name,-22}");
+                    Console.Write("|");
+                    Console.Write($"{students[i].HS1,-10}");
+                    Console.Write("|");
+                    Console.Write($"{students[i].HS2,-9}");
+                    Console.Write("|");
+                    Console.Write($"{students[i].tbkt,-10}");
+                    Console.Write("|");
+                    Console.Write($"{students[i].mark,-10}");
+                    Console.Write("|");
+                    Console.Write($"{students[i].tb,-10}");
+                    Console.Write("|");
+                    Console.WriteLine();
+                    Console.Write("|");
+                    for (int j = 0; j < 98; j++)
+                    {
+                        Console.Write("-");
+                    }
+                    Console.Write("|");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+     class Student
+    {
+        public String id { get; set; }
+        public String name { get; set; }
+        public int HS1 { get; set; }
+        public int HS2 { get; set; }
+        public float tbkt { get; set; }
+        public int mark { get; set; }
+        public float tb { get; set; }
+
+        public Student(string id, string name, int hS1, int hS2, float tbkt, int mark, float tb)
+        {
+            this.id = id;
+            this.name = name;
+            HS1 = hS1;
+            HS2 = hS2;
+            this.tbkt = tbkt;
+            this.mark = mark;
+            this.tb = tb;
+        }
+    }
+}
