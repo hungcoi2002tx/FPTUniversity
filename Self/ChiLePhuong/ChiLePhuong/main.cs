b45970ebc24c834896137ed09c85040090d4c462
Nguyen Minh Hung <hackinghungtx@gmail.com>
2023-05-16 20:23:20 +0700

    pc

---
+++ Self/ChiLePhuong/ChiLePhuong/main.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ChiLePhuong
+{
+    public class main
+    {
+        static void Main(string[] args)
+        {
+            Student student = new SeStudent(2, "kha", "SE");
+            Console.WriteLine(student.language());
+            
+
+        }
+        public static void DemoArrayList()
+        {   
+            //demo 1
+            ArrayList array = new ArrayList();
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+
+            int sum = (int)array[0] + (int)array[1];
+            Console.WriteLine(sum);
+
+            //demo 2
+            // co the add 1 object khac vao cung 1 arraylist. nhưng sẽ gặp nhiều khó khăn ở khâu lấy nó ra
+            ArrayList arr2 = new ArrayList();
+            arr2.Add(new Student(1,"hung"));
+            arr2.Add(new Student(2, "hai"));
+            arr2.Add(new Student(3, "hien"));
+            arr2.Add(1);
+            //((Student)arr2[3]).language(); // lỗi runtime
+        }
+
+        public static void DemoList()
+        {
+            List<int> list1 = new List<int>();
+            list1.Add(1);
+                list1.Add(2);                
+            list1.Add(3);
+            // list1.Add("acb"); lỗi compile
+            int sum = list1[0] + list1[1];
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Self/Generic/ConsoleApp1/VuKhi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	public class VuKhi
+	{
+        private String name;
+        public VuKhi() 
+		{
+			
+		}
+		public VuKhi(String name)
+		{
+
+		}
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+	public class SungLuc : VuKhi
+	{
+		public SungLuc()
+		{
+
+		}
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
